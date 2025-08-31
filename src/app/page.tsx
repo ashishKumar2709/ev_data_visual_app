a2358127c9f56a6b00be65cafb7deed4030f1fe0
Ashish Kumar <87716913+ashishKumar2709@users.noreply.github.com>
2025-08-31 10:44:33 +0530

    ui changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ const router = useRouter();
     <div className="h-full">
       <main className="h-full">
         <div className="h-full flex justify-center items-center">
-        <button className="border bg-gray-700 rounded p-4 cursor-pointer" onClick={()=>router.push("/graph")}>View Graphs</button>
+        <button className="border bg-gray-700 rounded p-4 cursor-pointer" onClick={()=>router.push("/graph")}>Dashboard</button>
       </div>
       </main>
       <footer className="">
